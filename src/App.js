3b461d53a00bce97f59e972e495ab832af9cfc7c
Christian Castillo <castillochristian3@gmail.com>
2024-01-17 10:25:08 -0500

    created FormSplitBill component in order to calculatre how the bill will be split

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -79,6 +80,31 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with friend name</h2>
+
+      <label> 💰 Bill value</label>
+      <input type="text" />
+
+      <label> 🧍🏼‍♂️Your expense</label>
+      <input type="text" />
+
+      <label>🧍🏻‍♀️ Friend's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">Friend</option>
+      </select>
+
+      <Button>Split Bill</Button>
     </form>
   );
 }
