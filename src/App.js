d45272625fc4d7730ebbeda79a39fa2fbbda1810
Christian Castillo <castillochristian3@gmail.com>
2024-01-18 11:27:47 -0500

    created paidByFriend variable in order to derive friend's expense

--- src/App.js
+++ src/App.js
@@ -155,6 +155,8 @@ function FormAddFriend({ onAddFriend }) {
 function FormSplitBill({ selectedFriend }) {
   const [bill, setBill] = useState('');
   const [paidByUser, setPaidByUser] = useState('');
+  const paidByFriend = bill ? bill - paidByUser : '';
+
   const [whoIsPaying, setWhoIsPaying] = useState('user');
 
   return (
@@ -176,7 +178,7 @@ function FormSplitBill({ selectedFriend }) {
       />
 
       <label>🧍🏻‍♀️ {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>🤑 Who is paying the bill</label>
       <select
